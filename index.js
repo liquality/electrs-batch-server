a33857edcf182ff794980a9a668159c6a65d381c
Harsh Vakharia <2311316+harshjv@users.noreply.github.com>
2022-02-09 16:25:41 +0530

    add cors middleware; rearrange middlewares; remove sentry

--- index.js
+++ index.js
@@ -6,48 +6,41 @@ const helmet = require('helmet')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const asyncHandler = require('express-async-handler')
-const Sentry = require('@sentry/node')
-const Tracing = require('@sentry/tracing')
+const cors = require('cors')
+
 const httpError = require('./http-error')
 const systemDefaults = require('./systemDefaults')
 
-const { NODE_ENV, ELECTRS_URL, SENTRY_DSN } = process.env
+const { NODE_ENV, ELECTRS_URL } = process.env
 const PORT = process.env.PORT || systemDefaults.port
 const CONCURRENCY = process.env.CONCURRENCY || systemDefaults.concurrency
 
 const app = express()
 
-if (NODE_ENV === 'production' && SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
-      new Tracing.Integrations.Express({ app })
-    ],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
-  })
-}
-
 if (!ELECTRS_URL) throw new Error('Invalid ELECTRS_URL')
 
-const electrs = axios.create({ baseURL: ELECTRS_URL, timeout: 30000 })
+const electrs = axios.create({ baseURL: ELECTRS_URL })
 
+app.use(
+  cors({
+    maxAge: 86400 // 1 day
+  })
+)
 app.use(helmet())
 app.use(compression())
 app.use(bodyParser.json({ limit: '5mb' }))
 app.set('etag', false)
 
+app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html')
+  res.send('Electrs Batch API is running')
+})
+
 // GET /status
 // A status endpoint for monitoring the batch API
 //   (on success) Returns status 200 and the latest indexed block
 //   (on error)   Returns the underlying error message with error status
-app.get('/status', async (req, res, next) => {
+app.get('/status', async (req, res) => {
   try {
     const payload = await electrs.get('/blocks/tip/height')
     const data = payload && payload.data ? payload.data : 'no data'
@@ -61,7 +54,7 @@ app.get('/status', async (req, res, next) => {
 
 app.post(
   '/addresses',
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
     let { addresses } = req.body
     if (!addresses || !_.isArray(addresses) || addresses.length === 0) {
       return res.status(400).json({ error: 'Invalid "addresses" field' })
@@ -83,7 +76,7 @@ app.post(
 
 app.post(
   '/addresses/utxo',
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
     let { addresses } = req.body
     if (!addresses || !_.isArray(addresses) || addresses.length === 0) {
       return res.status(400).json({ error: 'Invalid "addresses" field' })
@@ -108,7 +101,7 @@ app.post(
 
 app.post(
   '/addresses/transactions',
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
     let { addresses } = req.body
     if (!addresses || !_.isArray(addresses) || addresses.length === 0) {
       return res.status(400).json({ error: 'Invalid "addresses" field' })
@@ -129,27 +122,6 @@ app.post(
     res.json(response)
   })
 )
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'HEAD, GET, POST, OPTIONS')
-  next()
-})
-
-app.all('/*', function (req, res) {
-  res.setHeader('Content-Type', 'text/html')
-  res.send('Electrs Batch API is running')
-  // res.status(404).json({
-  //   error: '404'
-  // })
-})
-
-if (NODE_ENV === 'production') {
-  console.log('Setting production config')
-  app.use(Sentry.Handlers.requestHandler())
-  app.use(Sentry.Handlers.tracingHandler())
-  app.use(Sentry.Handlers.errorHandler())
-}
 
 app.use((err, req, res, next) => {
   const status = err.statusCode || 500
